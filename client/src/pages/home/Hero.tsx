cd1d37712a88eb2b8eb71a64f5171f39cd8efeb3
Jay Delisle <46854415+j-delisle@users.noreply.github.com>
2023-10-22 16:54:55 -0400

    Conditional rendering for homepage buttons per auth state

--- client/src/pages/home/Hero.tsx
+++ client/src/pages/home/Hero.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from 'react-router-dom';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 export default function Hero() {
+  const { user } = useAuthContext();
   return (
     <div className='bg-white'>
       <div className='px-6 py-24 sm:px-6 sm:py-32 lg:px-8'>
@@ -15,18 +17,22 @@ export default function Hero() {
             anim id veniam aliqua proident excepteur commodo do ea.
           </p>
           <div className='mt-10 flex flex-col items-center justify-center gap-y-6 w-full'>
-            <NavLink
-              to='/login'
-              className='w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-            >
-              Log In
-            </NavLink>
-            <NavLink
-              to='/signup'
-              className='w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-            >
-              Sign Up
-            </NavLink>
+            {!user && (
+              <>
+                <NavLink
+                  to='/login'
+                  className='w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                >
+                  Log In
+                </NavLink>
+                <NavLink
+                  to='/signup'
+                  className='w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                >
+                  Sign Up
+                </NavLink>
+              </>
+            )}
             <NavLink
               to='/leaderboard'
               className='w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
