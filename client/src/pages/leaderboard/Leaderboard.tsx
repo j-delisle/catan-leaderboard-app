1075896ecb5f5dc447c68a16070b77fda27f9d52
Jay Delisle <46854415+j-delisle@users.noreply.github.com>
2023-10-24 21:32:27 -0400

    boilerplate for leaderboard table added

--- client/src/pages/leaderboard/Leaderboard.tsx
+++ client/src/pages/leaderboard/Leaderboard.tsx
@@ -1,3 +1,10 @@
+import LeaderboardTable from './LeaderboardTable';
+
 export function Leaderboard() {
-  return <h1>Leaderboard Page</h1>;
+  return (
+    <>
+      <h1>Leaderboard Page</h1>
+      <LeaderboardTable />
+    </>
+  );
 }
